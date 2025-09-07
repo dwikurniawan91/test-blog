c97aa455786e42e071ec9c3ec33ae227a629631b
dwi.kur <dwi.kurniawan@youtap.id>
2025-09-07 12:47:04 +0700

    feat: fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
+import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  // base: 'https://github.com/dwikurniawan91/test-blog'
+export default defineConfig(({ command }) => {
+  const plugins = [tailwindcss(), reactRouter(), tsconfigPaths()];
+
+  if (command === 'build') {
+    return {
+      base: '/test-blog/',
+      plugins,
+    };
+  } else {
+    return {
+      plugins,
+    };
+  }
 });
+
