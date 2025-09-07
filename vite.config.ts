75a473953b5e466b8e7481d8b9c2e8abafb08517
dwi.kur <dwi.kurniawan@youtap.id>
2025-09-07 13:44:48 +0700

    fix: root meta

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,8 @@
-import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ command }) => {
-  const plugins = [tailwindcss(), reactRouter(), tsconfigPaths()];
-
-  if (command === 'build') {
-    return {
-      base: '/test-blog/',
-      plugins,
-    };
-  } else {
-    return {
-      plugins,
-    };
-  }
+export default defineConfig({
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
-
